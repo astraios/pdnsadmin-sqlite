b61e80987bfed9a8aa57563423b2844f5ccdb3ea
Thomas MICHEL <tmichel@doubletrade.com>
2017-10-05 11:16:11 +0200

    Fix entrypoint with some missing sed commands

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,8 +8,11 @@ SECRET_KEY=${SECRET_KEY:-A72AF848-B75D-402D-BA74-20C50B2BDAED}
 
 # Edit the configuration_template
 sed \
-  -e "s#\[ replace{'sqlite_path'} \]#${SQLITE_DBPATH}#g" \
+  -e "s#\[ replace{'sqlite_dbpath'} \]#${SQLITE_DBPATH}#g" \
   -e "s#\[ replace{'secret_key'} \]#${SECRET_KEY}#g" \
+  -e "s#\[ replace{'pdns_url'} \]#${PDNS_URL}#g" \
+  -e "s#\[ replace{'pdns_apikey'} \]#${PDNS_APIKEY}#g" \
+  -e "s#\[ replace{'pdns_version'} \]#${PDNS_VERSION}#g" \
   config_template.py > config.py
 
 # Start POwerDNS-Admin
